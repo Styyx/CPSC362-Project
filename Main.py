f22aad0c2d2d1ee2b0845ef7cbad05b6073fd1d4
CMier <caesarmier@csu.fullerton.edu>
2019-04-29 12:02:07 -0700

    teacherWindow
    opens new window but displays error message

--- Main.py
+++ Main.py
@@ -1,21 +1,10 @@
 import mysql.connector
 from mysql.connector import Error
 from tkinter import Tk, Label, Entry, Button, E, W
-
-def teacherLogin():
-    screen1 = Toplevel(my_gui)
-    screen1.title("Teacher Login")
-    screen1.geometry("300x250")
-
-    username = StringVar()
-    password = StringVar()
-
-    Label(text = "Username * ").pack()
-    Entry(textvariable = username)
-    Label(text = "Password * ").pack()
-    Entry(textvariable = password)
-    
+from tkinter import *
+   
 class logscreen:
+    
     def __init__(self, master):
         
         self.master = master
@@ -30,7 +19,7 @@ class logscreen:
         #self.text2 = Label(master, text="password")
         
         self.student_login_button = Button(master, text="Student Login")
-        self.teacher_login_button = Button(master, text="Teacher Login", command = teacherLogin)
+        self.teacher_login_button = Button(master, text="Teacher Login")
         self.account_register = Button(master, text="Register Account")
 
         self.title.grid(row=0, column=0, sticky=W)
@@ -45,8 +34,60 @@ class logscreen:
     def login(self):
         print("~")
 
+class logscreen:
+    
+    def __init__(self, master):
+        
+        self.master = master
+        master.title("Books Advantage Web App")
+
+        self.title = Label(master, text="Welcome to Books Advantage! Please sign in.")
+        
+        #self.username = Entry(master)
+        #self.text1 = Label(master, text="username")
+
+        #self.password = Entry(master)
+        #self.text2 = Label(master, text="password")
+        
+        self.student_login_button = Button(master, text="Student Login")
+        self.teacher_login_button = Button(master, text="Teacher Login", command = self.new_window)
+        self.account_register = Button(master, text="Register Account")
+
+        self.title.grid(row=0, column=0, sticky=W)
+        #self.text1.grid(row=1, column=0, sticky=W)
+        #self.username.grid(row=1, column=1, sticky=E)
+        #self.text2.grid(row=2, column=0, sticky=W)
+        #self.password.grid(row=2, column=1, sticky=E)
+        self.student_login_button.grid(row=3, column=0, sticky=W+E)
+        self.teacher_login_button.grid(row=4, column=0, sticky=W+E)
+        self.account_register.grid(row=5, column=0, sticky=W+E)
+    
+    def login(self):
+        print("~") 
+
+    def new_window(self):
+        self.newWindow = Toplevel(self.master)
+        self.my_gui = teacherWindow(self.newWindow)
+
+class teacherWindow:
+
+    def __init__(self, master):
+        username = StringVar()
+
+        password = StringVar()
+    
 
 
+        Label(text = "Username * ").pack()
+
+        Entry(textvariable = username)
+
+        Label(text = "Password * ").pack()
+
+        Entry(textvariable = password)
+
+
+    
 try:
     conn = mysql.connector.connect(host='127.0.0.1',
                              database='student',
