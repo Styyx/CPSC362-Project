6b54fc0b9535e7a68538da2bab500dc3ccbebbb1
Paul Rodriguez <https://PRodriguez-1996@github.com>
2019-05-11 15:18:49 -0700

    Added pictures, and new variables for login pages

--- new_gui.py
+++ new_gui.py
@@ -2,10 +2,12 @@ import tkinter
 from tkinter import Entry
 
 def stuLoginPg():
+    splash.pack_forget()
     stuLoginPgBtn.pack_forget()
     teaLoginPgBtn.pack_forget()
     adminLoginPgBtn.pack_forget()
     regAccPgBtn.pack_forget()
+    studentSplash.pack()
     emaillabel.pack()
     email.pack()
     passwordlabel.pack()
@@ -14,10 +16,12 @@ def stuLoginPg():
     stuCancelBtn.pack()
 
 def teaLoginPg():
+    splash.pack_forget()
     stuLoginPgBtn.pack_forget()
     teaLoginPgBtn.pack_forget()
     adminLoginPgBtn.pack_forget()
     regAccPgBtn.pack_forget()
+    teacherSplash.pack()
     emaillabel.pack()
     email.pack()
     passwordlabel.pack()
@@ -26,10 +30,12 @@ def teaLoginPg():
     teaCancelBtn.pack()
 
 def adminLoginPg():
+    splash.pack_forget()
     stuLoginPgBtn.pack_forget()
     teaLoginPgBtn.pack_forget()
     adminLoginPgBtn.pack_forget()
     regAccPgBtn.pack_forget()
+    adminSplash.pack()
     emaillabel.pack()
     email.pack()
     passwordlabel.pack()
@@ -38,10 +44,14 @@ def adminLoginPg():
     adminCancelBtn.pack()
 
 def regAccPg():
+    splash.pack_forget()
     stuLoginPgBtn.pack_forget()
     teaLoginPgBtn.pack_forget()
     adminLoginPgBtn.pack_forget()
     regAccPgBtn.pack_forget()
+    registerSplash.pack()
+    fullnameLabel.pack()
+    fullname.pack()
     emaillabel.pack()
     email.pack()
     passwordlabel.pack()
@@ -54,42 +64,51 @@ def regAccPg():
     
 
 def stuCancel():
+    studentSplash.pack_forget()
     emaillabel.pack_forget()
     email.pack_forget()
     passwordlabel.pack_forget()
     password.pack_forget()
     stuLoginBtn.pack_forget()
     stuCancelBtn.pack_forget()
+    splash.pack()
     stuLoginPgBtn.pack()
     teaLoginPgBtn.pack()
     adminLoginPgBtn.pack()
     regAccPgBtn.pack()
 
 def teaCancel():
+    teacherSplash.pack_forget()
     emaillabel.pack_forget()
     email.pack_forget()
     passwordlabel.pack_forget()
     password.pack_forget()
     teaLoginBtn.pack_forget()
     teaCancelBtn.pack_forget()
+    splash.pack()
     stuLoginPgBtn.pack()
     teaLoginPgBtn.pack()
     adminLoginPgBtn.pack()
     regAccPgBtn.pack()
 
 def adminCancel():
+    adminSplash.pack_forget()
     emaillabel.pack_forget()
     email.pack_forget()
     passwordlabel.pack_forget()
     password.pack_forget()
     adminLoginBtn.pack_forget()
     adminCancelBtn.pack_forget()
+    splash.pack()
     stuLoginPgBtn.pack()
     teaLoginPgBtn.pack()
     adminLoginPgBtn.pack()
     regAccPgBtn.pack()
 
 def regAccCancel():
+    registerSplash.pack_forget()
+    fullnameLabel.pack_forget()
+    fullname.pack_forget()
     emaillabel.pack_forget()
     email.pack_forget()
     passwordlabel.pack_forget()
@@ -99,6 +118,7 @@ def regAccCancel():
     regStuBtn.pack_forget()
     regTeaBtn.pack_forget()
     regAccCancelBtn.pack_forget()
+    splash.pack()
     stuLoginPgBtn.pack()
     teaLoginPgBtn.pack()
     adminLoginPgBtn.pack()
@@ -108,6 +128,7 @@ def stuLogin():
     #ideally we have some login verification programmed here
     #if the email and password fields are correct we continue to execute the code
     #if not we DO NOT execute the following code
+    studentSplash.pack_forget()
     emaillabel.pack_forget()
     email.pack_forget()
     passwordlabel.pack_forget()
@@ -121,6 +142,7 @@ def teaLogin():
     #ideally we have some login verification programmed here
     #if the email and password fields are correct we continue to execute the code
     #if not we DO NOT execute the following code
+    teacherSplash.pack_forget()
     emaillabel.pack_forget()
     email.pack_forget()
     passwordlabel.pack_forget()
@@ -134,6 +156,7 @@ def adminLogin():
     #ideally we have some login verification programmed here
     #if the email and password fields are correct we continue to execute the code
     #if not we DO NOT execute the following code
+    adminSplash.pack_forget()
     emaillabel.pack_forget()
     email.pack_forget()
     passwordlabel.pack_forget()
@@ -146,6 +169,7 @@ def adminLogin():
 def stuLogout():
     stuPg.pack_forget()
     stuLogoutBtn.pack_forget()
+    studentSplash.pack()
     emaillabel.pack()
     email.pack()
     passwordlabel.pack()
@@ -156,6 +180,7 @@ def stuLogout():
 def teaLogout():
     teaPg.pack_forget()
     teaLogoutBtn.pack_forget()
+    teacherSplash.pack()
     emaillabel.pack()
     email.pack()
     passwordlabel.pack()
@@ -166,52 +191,76 @@ def teaLogout():
 def adminLogout():
     adminPg.pack_forget()
     adminLogoutBtn.pack_forget()
+    adminSplash.pack()
     emaillabel.pack()
     email.pack()
     passwordlabel.pack()
     password.pack()
     adminLoginBtn.pack()
     adminCancelBtn.pack()
+
+#this is the function that will contain the logic for registering a Student, feel free to delete the print statement, I just put it there so the module would run during my testing
+def regStu():
+    print("placeholder")
+
+#this is the function that will contain the logic for registering a teacher, feel free to delete the print statement, I just put it there so the module would run during my testing
+def regTea():
+    print("placeholder")
     
 
 window = tkinter.Tk()
-
+window.geometry("1000x500")
 #the following buttons need logic assigned to them, stuLoginBtn, teaLoginBtn, adminLoginBtn regStuBtn, and regTeaBtn
 
-stuLoginPgBtn = tkinter.Button(window, text="Student Login", command=stuLoginPg)
-teaLoginPgBtn = tkinter.Button(window, text="Teacher Login", command=teaLoginPg)
-adminLoginPgBtn = tkinter.Button(window, text="Admin Login", command=adminLoginPg)
-regAccPgBtn = tkinter.Button(window, text="Register an Account", command=regAccPg)
+stuLoginPgBtn = tkinter.Button(window, text="Student Login", bg="red", fg="white", command=stuLoginPg)
+teaLoginPgBtn = tkinter.Button(window, text="Teacher Login", bg="green", fg="black", command=teaLoginPg)
+adminLoginPgBtn = tkinter.Button(window, text="Admin Login", bg="blue", fg="white", command=adminLoginPg)
+regAccPgBtn = tkinter.Button(window, text="Register an Account", bg="yellow", fg="black", command=regAccPg)
 
-stuLoginBtn = tkinter.Button(window, text="Login", command=stuLogin)
-teaLoginBtn = tkinter.Button(window, text="Login", command=teaLogin)
-adminLoginBtn = tkinter.Button(window, text="Login", command=adminLogin)
+stuLoginBtn = tkinter.Button(window, text="Login", bg="red", fg="white", command=stuLogin)
+teaLoginBtn = tkinter.Button(window, text="Login", bg="green", fg="black", command=teaLogin)
+adminLoginBtn = tkinter.Button(window, text="Login", bg="blue", fg="white", command=adminLogin)
 
-stuLogoutBtn = tkinter.Button(window, text="Logout", command=stuLogout)
-teaLogoutBtn = tkinter.Button(window, text="Logout", command=teaLogout)
-adminLogoutBtn = tkinter.Button(window, text="Logout", command=adminLogout)
+stuLogoutBtn = tkinter.Button(window, text="Logout",  bg="red", fg="white", command=stuLogout)
+teaLogoutBtn = tkinter.Button(window, text="Logout", bg="green", fg="black", command=teaLogout)
+adminLogoutBtn = tkinter.Button(window, text="Logout", bg="blue", fg="white", command=adminLogout)
 
-stuCancelBtn = tkinter.Button(window, text="Cancel", command=stuCancel)
-teaCancelBtn = tkinter.Button(window, text="Cancel", command=teaCancel)
-adminCancelBtn = tkinter.Button(window, text="Cancel", command=adminCancel)
-regAccCancelBtn = tkinter.Button(window, text="Cancel", command=regAccCancel)
+stuCancelBtn = tkinter.Button(window, text="Cancel", bg="red", fg="white", command=stuCancel)
+teaCancelBtn = tkinter.Button(window, text="Cancel", bg="green", fg="black", command=teaCancel)
+adminCancelBtn = tkinter.Button(window, text="Cancel",  bg="blue", fg="white", command=adminCancel)
+regAccCancelBtn = tkinter.Button(window, text="Cancel", bg="yellow", fg="black", command=regAccCancel)
 
-regStuBtn = tkinter.Button(window, text="Register as Student") 
-regTeaBtn = tkinter.Button(window, text="Register as Teacher")
-regAdminBtn =tkinter.Button(window, text="Register as Admin")
-
+regStuBtn = tkinter.Button(window, text="Register as Student", bg="red", fg="white", command=regStu) 
+regTeaBtn = tkinter.Button(window, text="Register as Teacher", bg="green", fg="black", command=regTea)
 
 email = Entry(window) #this is the variable containing the email field
 emaillabel = tkinter.Label(window, text="Email:")
 password = Entry(window, show="*") #this is the variable containing the password field
 passwordlabel = tkinter.Label(window, text="Password:")
-cPassword = Entry(window)
+cPassword = Entry(window, show="*") #this is the variable containing the confirm password field on the registration page
 cPasswordLabel = tkinter.Label(window, text="Confirm Password:")
+fullname = Entry(window)
+fullnameLabel = tkinter.Label(window, text= "Full Name:")
 
-stuPg = tkinter.Label(window, text="Welcome to the student page. (This is a placeholder)")
-teaPg = tkinter.Label(window, text="Welcome to the teacher page. (This is a placeholder)")
-adminPg = tkinter.Label(window, text="Welcome to the admin page. (This is a placeholder)")
+studentName = "placeholder" #this is the variable containing the student logging in
+stuPg = tkinter.Label(window, text="Welcome %s" % (studentName))
+teacherName = "placeholder" #this is the variable containing the teacher logging in
+teaPg = tkinter.Label(window, text="Welcome %s" % (teacherName))
+adminName = "placeholder" #this is the variable containing the admin logging in
+adminPg = tkinter.Label(window, text="Welcome %s" % (adminName))
 
+logo= tkinter.PhotoImage(file ="books.png")
+splash = tkinter.Label(window, image=logo)
+studentLogo= tkinter.PhotoImage(file="studentlogin.png")
+studentSplash = tkinter.Label(window, image=studentLogo)
+teacherLogo= tkinter.PhotoImage(file="teacherlogin.png")
+teacherSplash = tkinter.Label(window, image=teacherLogo)
+adminLogo= tkinter.PhotoImage(file="adminlogin.png")
+adminSplash = tkinter.Label(window, image=adminLogo)
+registerLogo= tkinter.PhotoImage(file="registerpage.png")
+registerSplash = tkinter.Label(window, image=registerLogo)
+
+splash.pack()
 stuLoginPgBtn.pack()
 teaLoginPgBtn.pack()
 adminLoginPgBtn.pack()
